167032b1566e7d5cb8f048788f0d93f6aed0fba5
Nikitosios <nikita.diachina@yandex.ru>
2016-12-23 19:21:34 +0200

    initial commit

---
+++ devices/devices.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <libusb.h>
+
+int main (const int argc, const char ** const argv)
+{
+	
+	return 0;
+}
---
+++ files_advanced/files_very_advanced.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (const int const argc, const char ** argv)
+{
+	FILE *fi, *fo;
+	unsigned long size;
+
+	fi = fopen(argv[1], "r");
+	fo = fopen(argv[2], "w");
+	fseek(fi, 0, SEEK_END);
+	size = ftell(fi);
+	for (long i = 0; i < size; i++)
+		putc(getc(fi), fo);
+	fclose(fi);
+	fclose(fo);
+	return 0;
+}
