6226160d96c89c4d3e34c41d2ef5d9aee1edf450
Nikitosios <nikita.diachina@yandex.ru>
2016-12-23 21:49:52 +0200

    Test commmit from vim

--- svastony/svastony.c
+++ svastony/svastony.c
@@ -13,7 +13,7 @@ int main (int argc, char ** argv) {
     label = gtk_label_new("Здравствуй, мир!");        // Создаем метку с текстом
     gtk_container_add(GTK_CONTAINER(window), label);         // Вставляем метку в главное окно
     gtk_widget_show_all(window);                             // Показываем окно вместе с виджетами
-    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL); // Соединяем сигнал завершения с выходом
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_main();                                              // Приложение переходит в цикл ожидания
     return 0; 
 }
