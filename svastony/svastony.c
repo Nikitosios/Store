a9847230a6c467414c6f40c498b4cfe8c03c299d
nikitosios <nikita.diachina@yandex.ru>
2016-12-23 21:52:18 +0200

    Test commit from vim

--- svastony/svastony.c
+++ svastony/svastony.c
@@ -9,11 +9,12 @@ int main (int argc, char ** argv) {
 
     gtk_init(&argc, &argv);                           // Инициализируем GTK+
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);            // Создаем главное окно
-    gtk_window_set_title(GTK_WINDOW(window), TITLE);         // Устанавливаем заголовок окна 
+    gtk_window_set_title(GTK_WINDOW(window), TITLE);         // Устанавливаем заголовок окна
     label = gtk_label_new("Здравствуй, мир!");        // Создаем метку с текстом
     gtk_container_add(GTK_CONTAINER(window), label);         // Вставляем метку в главное окно
     gtk_widget_show_all(window);                             // Показываем окно вместе с виджетами
     g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_main();                                              // Приложение переходит в цикл ожидания
-    return 0; 
+    return 0;
 }
+
