018b28ad9547fc6a3c0903777fa4c504642fd405
Nikitosios <nikita.diachina@yandex.ru>
2016-12-31 16:12:47 +0200

    bugfix

--- app/main.c
+++ app/main.c
@@ -18,6 +18,7 @@ int main (int argc, char *argv[])
 	int ret = 0;
 
 	gtk_init(&argc, &argv);
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(window), title);
 	gtk_container_set_border_width(GTK_CONTAINER(window), border_width);
 	grid = gtk_grid_new();
