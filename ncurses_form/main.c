63f7356431f8a7893bf85f893e03744ec2261771
nikitosios <nikita.diachina@yandex.ru>
2017-01-10 23:24:51 +0200

    update

--- ncurses_form/main.c
+++ ncurses_form/main.c
@@ -31,6 +31,8 @@ struct object create_object(int x, int y, int w, int h)
 int posx = 0, posy = 0;
 struct object msgbox;
 struct object msgsend;
+struct object sendfile;
+struct object button_exit;
 
 int main (void)
 {
@@ -53,6 +55,19 @@ int main (void)
 			case '\n':
 				posx = 0;
 				posy++;
+				break;
+			case KEY_BACKSPACE:
+				init_pair(7, COLOR_BLACK, COLOR_WHITE);
+				if(posx - 1 >= 0) {
+					mvaddch(posy + msgbox.y, posx + msgbox.x, ' ' | COLOR_PAIR(7));
+					posx--;
+					move(posy + 1 + msgbox.y, posx + 1 + msgbox.x);
+				} else if(posy - 1 >= 0) {
+					mvaddch(posy - 1 + msgbox.y, posx + msgbox.x, ' ' | COLOR_PAIR(7));
+					posx = 0;
+					posy--;
+					move(posy + 1 + msgbox.y, posx + 1 + msgbox.x);
+				}
 				break;
 			case KEY_MOUSE:
 				if (getmouse(&event) == OK)
@@ -81,8 +96,11 @@ int show_all (int a, int b)
 	init_pair(2, a, b);
 	init_pair(3, COLOR_RED, COLOR_RED);
 	init_pair(4, COLOR_BLACK, COLOR_RED);
+	init_pair(9, COLOR_BLACK, COLOR_BLUE);
+	init_pair(11, COLOR_BLACK, COLOR_WHITE);
+	init_pair(12, COLOR_WHITE, COLOR_BLUE);
 	attrset(COLOR_PAIR(3));
-	struct object button_exit = create_object(COLS/8*7-5, 0, 5, 3);
+	button_exit = create_object(COLS/8*7-5, 0, 5, 3);
 	for (int i = button_exit.y; i < button_exit.h + button_exit.y; i++)
 		for (int it = button_exit.x; it < button_exit.w + button_exit.x; it++)
 			mvaddch(i, it, ' ');
@@ -98,7 +116,28 @@ int show_all (int a, int b)
 	for (int i = msgbox.x; i < msgbox.x + msgbox.w; i++)
 		for (int it = msgbox.y; it < msgbox.y + msgbox.h; it++)
 			mvaddch(it, i, ' ' | COLOR_PAIR(7));
-	msgbox_refresh();
+	init_pair(7, COLOR_BLACK, COLOR_WHITE);
+	for (int i = msgbox.y; i < msgbox.h + msgbox.y; i++)
+		mvaddch(i, msgbox.x, '|' | A_BOLD | COLOR_PAIR(7));
+	for (int i = msgbox.y; i < msgbox.y + msgbox.h; i++)
+		mvaddch(i, msgbox.x + msgbox.w, '|' | A_BOLD | COLOR_PAIR(7));
+	sendfile = create_object(0, msgbox.y, 5, LINES - msgbox.y);
+	for (int i = sendfile.x; i < sendfile.x + sendfile.w; i++)
+		for (int it = sendfile.y; it <= sendfile.y + sendfile.h; it++)
+			mvaddch(it, i, ' ' | A_BOLD | COLOR_PAIR(12));
+	mvaddch(sendfile.y+sendfile.h/2, sendfile.w/2+sendfile.x, '#' | COLOR_PAIR(9) | A_BOLD);
+	for (int i = 0; i < msgsend.x; i++)
+		mvaddch(msgbox.y, i, '-' | COLOR_PAIR(11) | A_BOLD);
+	for (int i = 0; i < msgsend.x; i++)
+		mvaddch(LINES-1, i, '-' | COLOR_PAIR(11) | A_BOLD);
+	mvaddch(msgbox.y, msgbox.x, 192 | COLOR_PAIR(11) | A_BOLD);
+	mvaddch(LINES-1, msgbox.x, 218 | COLOR_PAIR(11) | A_BOLD);
+	move (LINES/2, COLS/2);
+	addch(218 | A_NORMAL);
+	for (int i = 0; i < msgbox.x; i++) {
+		mvaddch(msgbox.y, i, '-' | COLOR_PAIR(12));
+		mvaddch(LINES-1, i, '-' | COLOR_PAIR(12));
+	}
 	refresh();
 	return 0;
 }
@@ -122,25 +161,10 @@ int show_char (char c)
 		posy++;
 		posx = 0;
 	}
-	init_pair(8, COLOR_BLACK, COLOR_WHITE);
-	mvaddch(posy+msgbox.y+1, posx+msgbox.x+1, c | COLOR_PAIR(8));
+	init_pair(10, COLOR_BLACK, COLOR_WHITE);
+	mvaddch(posy+msgbox.y+1, posx+msgbox.x+1, c | COLOR_PAIR(10));
 	posx++;
+	move(posy + msgbox.y + 1, posx + 1 + msgbox.x);
 	refresh();
-	return 0;
-}
-
-int msgbox_refresh (void)
-{
-	extern struct object msgbox;
-
-	init_pair(7, COLOR_BLACK, COLOR_WHITE);
-	for (int i = msgbox.x; i < msgbox.w + msgbox.x; i++)
-		mvaddch(msgbox.y, i, '-' | A_BOLD | COLOR_PAIR(7));
-	for (int i = msgbox.x; i < msgbox.x + msgbox.w; i++)
-		mvaddch(msgbox.y + msgbox.h, i, '-' | A_BOLD | COLOR_PAIR(7));
-	for (int i = msgbox.y; i < msgbox.h + msgbox.y; i++)
-		mvaddch(i, msgbox.x, '|' | A_BOLD | COLOR_PAIR(7));
-	for (int i = msgbox.y; i < msgbox.y + msgbox.h; i++)
-		mvaddch(i, msgbox.x + msgbox.w, '|' | A_BOLD | COLOR_PAIR(7));
 	return 0;
 }
