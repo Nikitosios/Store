7a9c554b96ccecc3ce5ff280cc29ad8ff0e80219
nikitosios <nikita.diachina@yandex.ru>
2017-01-11 22:08:56 +0200

    update

--- ncurses/src/main.c
+++ ncurses/src/main.c
@@ -1,18 +1,27 @@
 #include <ncurses.h>
 #include <locale.h>
+#include <form.h>
 
 int update_screen (void);
 
 int main (void)
 {
+	int maxy, maxx;
+
+	/* initialize all */
 	setlocale(LC_ALL, "");
 	initscr();
-	mousemask(ALL_MOUSE_EVENTS, NULL);
-	keypad(stdscr, TRUE);
 	cbreak();
+	noecho();
+	keypad(stdscr, TRUE);
 	start_color();
-	getch();
+	mousemask(ALL_MOUSE_EVENTS, NULL);
+	/* main code */
 	update_screen();
+	/* end program */
+	clear();
+	getmaxyx(stdscr, maxy, maxx);
+	mvprintw(maxy/2, maxx/2-8, "Press any key...");
 	getch();
 	endwin();
 	return 0;
