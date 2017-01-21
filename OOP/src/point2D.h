dfaaddf09885ee9a1e8bffe815f845157d971293
nikitosios <nikita.diachina@yandex.ru>
2017-01-21 23:07:44 +0200

    update

--- OOP/src/point2D.h
+++
@@ -1,7 +0,0 @@
-typedef struct point2D {
-	void *prvtPoint2D;
-	int (*getX) (struct point2D*);
-	void (*setX)(struct point2D*, int);
-} point2D;
-point2D* newPoint2D (void);
-void deletePoint2D (point2D*);
